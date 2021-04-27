9b921e422d7d0f483546e646fcd6c2d167501f24
Craig Miller <craigmiller160@gmail.com>
2021-04-27 15:43:35 -0400

    Code cleanup

--- test/tradier/tradier.e2e-spec.ts
+++ test/tradier/tradier.e2e-spec.ts
@@ -2,14 +2,11 @@ import { HttpService, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../src/app.module';
 import request from 'supertest';
-import MockAdapter from 'axios-mock-adapter';
-import axios, { AxiosResponse } from 'axios';
+import { AxiosResponse } from 'axios';
 import { of } from 'rxjs';
 import quotes from './__data__/quotes.json';
 
-const mockApi = new MockAdapter(axios);
-
-const createResponse = (data: any): AxiosResponse => ({
+const createResponse = <T>(data: T): AxiosResponse<T> => ({
 	data,
 	headers: {},
 	config: {},
@@ -29,14 +26,9 @@ describe('TradierController (e2e)', () => {
 		app = moduleFixture.createNestApplication();
 		httpService = app.get<HttpService>(HttpService);
 		await app.init();
-
-		mockApi.reset();
 	});
 
 	it('GET /tradier/quote/:symbol', () => {
-		mockApi
-			.onGet('https://sandbox.tradier.com/v1/markets/quotes?symbols=AAPL')
-			.reply(200, 'Success');
 		const spy = jest.spyOn(httpService, 'get');
 		spy.mockImplementationOnce(() => of(createResponse(quotes)));
 
