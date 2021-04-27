81a16353367157977dc9c4982107fa8ff2e368c3
Craig Miller <craigmiller160@gmail.com>
2021-04-27 13:59:17 -0400

    Stubbing out API

---
+++ src/tradier/tradier.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('/tradier')
+export class TradierController {
+
+    @Get('/quote')
+    getStockQuote(symbol: string) {
+        // TODO finish this
+    }
+
+    @Get('/quote/history')
+    getStockHistoryQuote(symbol: string, date: string) {
+        // TODO finish this
+    }
+}
