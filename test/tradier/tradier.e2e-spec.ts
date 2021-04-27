85942de71304f41c7e1ac9a2c28dd1a6773233d0
Craig Miller <craigmiller160@gmail.com>
2021-04-27 15:41:32 -0400

    Got first test working

--- test/tradier/tradier.e2e-spec.ts
+++ test/tradier/tradier.e2e-spec.ts
@@ -1,9 +1,25 @@
-import { INestApplication } from '@nestjs/common';
+import { HttpService, INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../../src/app.module';
+import request from 'supertest';
+import MockAdapter from 'axios-mock-adapter';
+import axios, { AxiosResponse } from 'axios';
+import { of } from 'rxjs';
+import quotes from './__data__/quotes.json';
+
+const mockApi = new MockAdapter(axios);
+
+const createResponse = (data: any): AxiosResponse => ({
+	data,
+	headers: {},
+	config: {},
+	status: 200,
+	statusText: 'OK'
+});
 
 describe('TradierController (e2e)', () => {
 	let app: INestApplication;
+	let httpService: HttpService;
 
 	beforeEach(async () => {
 		const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -11,11 +27,23 @@ describe('TradierController (e2e)', () => {
 		}).compile();
 
 		app = moduleFixture.createNestApplication();
+		httpService = app.get<HttpService>(HttpService);
 		await app.init();
+
+		mockApi.reset();
 	});
 
 	it('GET /tradier/quote/:symbol', () => {
-		throw new Error();
+		mockApi
+			.onGet('https://sandbox.tradier.com/v1/markets/quotes?symbols=AAPL')
+			.reply(200, 'Success');
+		const spy = jest.spyOn(httpService, 'get');
+		spy.mockImplementationOnce(() => of(createResponse(quotes)));
+
+		return request(app.getHttpServer())
+			.get('/tradier/quote/AAPL')
+			.expect(200)
+			.expect(quotes.quotes.quote);
 	});
 
 	it('GET /tradier/quote/history/:symbol/:date', () => {
