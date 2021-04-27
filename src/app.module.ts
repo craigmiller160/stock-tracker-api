6aa45dabb8085e862b21a71375b5b736ac2fd0c0
Craig Miller <craigmiller160@gmail.com>
2021-04-27 14:49:59 -0400

    Integrated TradierModule into application

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TradierModule } from './tradier/tradier.module';
 
 // TODO AppController & AppService can go away
 @Module({
@@ -9,7 +10,8 @@ import { ConfigModule } from '@nestjs/config';
 		ConfigModule.forRoot({
 			envFilePath: ['.env', '.env.private'],
 			isGlobal: true
-		})
+		}),
+		TradierModule
 	],
 	controllers: [AppController],
 	providers: [AppService]
