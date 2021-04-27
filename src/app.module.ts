22ac7cb897190183e217c72e72afc18ed71d3990
Craig Miller <craigmiller160@gmail.com>
2021-04-27 13:52:37 -0400

    Setup ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
+// TODO AppController & AppService can go away
 @Module({
-  imports: [],
+  imports: [
+      ConfigModule.forRoot({
+        envFilePath: [
+            '.env',
+            '.env.private'
+        ]
+      })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
