c672985945332f851f97e65244bae0e2810a03e1
Craig Miller <craigmiller160@gmail.com>
2021-04-28 16:39:58 -0400

    Properly typed AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,16 @@
 import { Controller, Post, Request, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { LocalAuthGuard } from '../guards/LocalAuthGuard';
+import { User } from '../user/model/user';
+
+interface RequestWithUser extends Request {
+	user: User;
+}
 
 @Controller('/auth')
 export class AuthController {
-
-    @UseGuards(AuthGuard('local'))
-    @Post('/login')
-    login(@Request() req): any {
-        return req.user;
-    }
+	@UseGuards(LocalAuthGuard)
+	@Post('/login')
+	login(@Request() req: RequestWithUser): User {
+		return req.user;
+	}
 }
