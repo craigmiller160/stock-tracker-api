7400f3c211e9d3c5d0dd014ae14cf67ada356289
Craig <craigmiller160@gmail.com>
2021-04-28 19:18:32 -0400

    Stubbed out ability to enhance JwtAuthGuard

--- src/guards/JwtAuthGuard.ts
+++ src/guards/JwtAuthGuard.ts
@@ -1,5 +1,20 @@
 import { AuthGuard } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+	canActivate(
+		context: ExecutionContext
+	): boolean | Promise<boolean> | Observable<boolean> {
+		return super.canActivate(context);
+	}
+
+	// handleRequest(err: any, user: any, info: any) {
+	// 	// You can throw an exception based on either "info" or "err" arguments
+	// 	if (err || !user) {
+	// 		throw err || new UnauthorizedException();
+	// 	}
+	// 	return user;
+	// }
+}
