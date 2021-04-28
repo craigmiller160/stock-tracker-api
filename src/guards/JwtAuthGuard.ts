3a534e5835879854e7757c8ae7d7a32e5f97574f
Craig <craigmiller160@gmail.com>
2021-04-28 19:27:17 -0400

    Allowed requests to go to the login endpoint again

--- src/guards/JwtAuthGuard.ts
+++ src/guards/JwtAuthGuard.ts
@@ -1,13 +1,24 @@
 import { AuthGuard } from '@nestjs/passport';
 import { ExecutionContext, Injectable } from '@nestjs/common';
 import { Observable } from 'rxjs';
+import { Reflector } from '@nestjs/core';
+import { IS_INSECURE_KEY } from './Insecure';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+	constructor(private readonly reflector: Reflector) {
+		super();
+	}
+
 	canActivate(
 		context: ExecutionContext
 	): boolean | Promise<boolean> | Observable<boolean> {
-		return super.canActivate(context);
+		const isInsecure = this.reflector.getAllAndOverride<boolean>(
+			IS_INSECURE_KEY,
+			[context.getHandler(), context.getClass()]
+		);
+
+		return isInsecure || super.canActivate(context);
 	}
 
 	// handleRequest(err: any, user: any, info: any) {
