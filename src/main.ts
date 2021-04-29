9f5fb2b8ec5471f66019855a327a15dd76b5a6a8
Craig <craigmiller160@gmail.com>
2021-04-28 21:30:36 -0400

    Added key & cert to Nest

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import fs from 'fs';
+import path from 'path';
+import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
+
+const keyPath = path.resolve(__dirname, 'cert', 'stock-tracker.key.pem');
+const certPath = path.resolve(__dirname, 'cert', 'stock-tracker.cert.pem');
+
+const httpsOptions: HttpsOptions = {
+	key: fs.readFileSync(keyPath),
+	cert: fs.readFileSync(certPath)
+};
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule, {
+		httpsOptions
+	});
 	await app.listen(3000);
 }
 bootstrap();
