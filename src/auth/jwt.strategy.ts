5ddf4b1d6f8fa54e8a443f6476c35b02899aefd3
Craig <craigmiller160@gmail.com>
2021-04-28 21:52:10 -0400

    Wired up new service

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,15 +4,17 @@ import { jwtConstants } from './constants';
 import { Claims } from './model/jwt';
 import { User } from '../user/model/user';
 import { Injectable } from '@nestjs/common';
+import { JwkService } from './jwk.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-	constructor() {
+	constructor(private readonly jwkService: JwkService) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
 			secretOrKey: jwtConstants.secret
 		});
+		console.log(jwkService.message); // TODO delete this
 	}
 
 	validate(payload: Claims): User {
