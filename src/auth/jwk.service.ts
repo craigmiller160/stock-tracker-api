18491c13b30ff30876ce083918c1650a102f83ee
Craig <craigmiller160@gmail.com>
2021-04-30 13:32:03 -0400

    Making some progress

--- src/auth/jwk.service.ts
+++ src/auth/jwk.service.ts
@@ -1,9 +1,12 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
+import { createRemoteJWKSet } from 'jose/jwks/remote'
 
+// TODO make the jwkset url configurable
 @Injectable()
 export class JwkService implements OnModuleInit {
 
 	onModuleInit(): void {
-
+		const jwkSet = createRemoteJWKSet(new URL('https://localhost:7003/jwk'));
+		console.log('JwkSet', jwkSet.getKey('')); // TODO delete this
 	}
 }
