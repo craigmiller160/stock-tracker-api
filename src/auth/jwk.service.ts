92d4bd97e1ee8bb99e1f9ba4f7bc054704c7cd60
Craig <craigmiller160@gmail.com>
2021-04-30 08:25:43 -0400

    Starting to work on how to load JWKSet

--- src/auth/jwk.service.ts
+++ src/auth/jwk.service.ts
@@ -7,5 +7,6 @@ export class JwkService implements OnModuleInit {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	onModuleInit(): any {
 		this.message = 'Init done';
+		console.log('Message set'); // TODO delete this
 	}
 }
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -4,17 +4,15 @@ import { jwtConstants } from './constants';
 import { Claims } from './model/jwt';
 import { User } from '../user/model/user';
 import { Injectable } from '@nestjs/common';
-import { JwkService } from './jwk.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-	constructor(private readonly jwkService: JwkService) {
+	constructor() {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
 			secretOrKey: jwtConstants.secret
 		});
-		console.log(jwkService.message); // TODO delete this
 	}
 
 	validate(payload: Claims): User {
