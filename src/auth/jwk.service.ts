3a288d649f9e00011af66fef0dc38ab9b290fb9f
Craig <craigmiller160@gmail.com>
2021-04-30 15:20:34 -0400

    Barely any progress since my last commit

--- src/auth/jwk.service.ts
+++ src/auth/jwk.service.ts
@@ -1,9 +1,11 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { createRemoteJWKSet } from 'jose/jwks/remote'
+import { BehaviorSubject } from 'rxjs';
 
 // TODO make the jwkset url configurable
 @Injectable()
 export class JwkService implements OnModuleInit {
+	jwkSet = new BehaviorSubject<null>(null); // TODO fix the type here
 
 	onModuleInit(): void {
 		const jwkSet = createRemoteJWKSet(new URL('https://localhost:7003/jwk'));
