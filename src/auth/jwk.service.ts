62cf6485c4dee72e651ea316373f6c16e98f4edb
Craig <craigmiller160@gmail.com>
2021-04-30 16:00:04 -0400

    Got the subject setup properly

--- src/auth/jwk.service.ts
+++ src/auth/jwk.service.ts
@@ -1,20 +1,38 @@
-import { HttpService, Injectable, OnModuleInit } from '@nestjs/common';
-import { BehaviorSubject } from 'rxjs';
+import {
+	HttpService,
+	Injectable,
+	OnModuleDestroy,
+	OnModuleInit
+} from '@nestjs/common';
+import { BehaviorSubject, Subscription } from 'rxjs';
 import { AxiosResponse } from 'axios';
 import { JwkSet } from './model/jwk';
 import jwkToPem from 'jwk-to-pem';
+import { map } from 'rxjs/operators';
 
 const jwkUrl = 'https://localhost:7003/jwk'; // TODO add to configuration
 
+// TODO need error handling that kills whole app
+
 @Injectable()
-export class JwkService implements OnModuleInit {
-	jwkSet = new BehaviorSubject<null>(null); // TODO fix the type here
+export class JwkService implements OnModuleInit, OnModuleDestroy {
+	key = new BehaviorSubject<null>(null); // TODO fix the type here
+	private subscription: Subscription;
 
 	constructor(private httpService: HttpService) {}
 
 	onModuleInit(): void {
-		this.httpService.get(jwkUrl).subscribe((res: AxiosResponse<JwkSet>) => {
-			const pem = jwkToPem(res.data.keys[0]);
-		});
+		this.subscription = this.httpService
+			.get(jwkUrl)
+			.pipe(
+				map((res: AxiosResponse<JwkSet>) => jwkToPem(res.data.keys[0]))
+			)
+			.subscribe(this.key);
+	}
+
+	onModuleDestroy(): void {
+		if (this.subscription) {
+			this.subscription.unsubscribe();
+		}
 	}
 }
