13ab3bbf0ec3c2cfa31d97c4e3f59d97671f35f3
Craig <craigmiller160@gmail.com>
2021-04-30 15:36:27 -0400

    Ok, got the jwkset downloaded

--- src/auth/jwk.service.ts
+++ src/auth/jwk.service.ts
@@ -1,8 +1,5 @@
 import { HttpService, Injectable, OnModuleInit } from '@nestjs/common';
-import { createRemoteJWKSet } from 'jose/jwks/remote'
 import { BehaviorSubject } from 'rxjs';
-import axios from 'axios';
-import * as https from 'https';
 
 const jwkUrl = 'https://localhost:7003/jwk';
 
@@ -14,19 +11,11 @@ export class JwkService implements OnModuleInit {
 	constructor(private httpService: HttpService) {}
 
 	onModuleInit(): void {
-		console.log('Initializing'); // TODO delete this
 		// const jwkSet = createRemoteJWKSet(new URL('https://localhost:7003/jwk'));
-		// this.httpService.get(jwkUrl)
-		// 	.subscribe((value) => {
-		// 		console.log('In subscription');
-		// 		console.log(value);
-		// 	});
-		axios.get(jwkUrl, {
-			httpsAgent: new https.Agent({
-				rejectUnauthorized: false
-			})
-		})
-			.then((res) => console.log(res.data))
-			.catch((ex) => console.error(ex.message));
+		this.httpService.get(jwkUrl)
+			.subscribe((value) => {
+				console.log('In subscription');
+				console.log(value.data);
+			});
 	}
 }
