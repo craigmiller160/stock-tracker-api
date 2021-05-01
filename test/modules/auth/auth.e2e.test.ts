821c9d21d1cb4c5b0166d6094b57fc6caf9da449
Craig <craigmiller160@gmail.com>
2021-05-01 12:53:17 -0400

    Committing all progress

--- test/modules/auth/auth.e2e.test.ts
+++ test/modules/auth/auth.e2e.test.ts
@@ -4,6 +4,13 @@ import { INestApplication, OnModuleInit } from '@nestjs/common';
 import request, { Response } from 'supertest';
 import atob from 'atob';
 import { JwkService } from '../../../src/modules/auth/jwk.service';
+import { JwtService } from '@nestjs/jwt';
+
+const tokenKey = 'TokenKey';
+const token = {
+	clientKey: 'clientKey',
+	clientName: 'clientName'
+};
 
 class MockJwkService extends JwkService implements OnModuleInit {
 	onModuleInit(): void {
@@ -13,6 +20,7 @@ class MockJwkService extends JwkService implements OnModuleInit {
 
 describe('AuthController (e2e)', () => {
 	let app: INestApplication;
+	let signedToken: string;
 
 	beforeEach(async () => {
 		const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -23,12 +31,22 @@ describe('AuthController (e2e)', () => {
 			.compile();
 
 		app = moduleFixture.createNestApplication();
+
+		const jwkService = app.get<JwkService>(JwkService);
+		jwkService.key.next(tokenKey);
+
+		const jwtService = app.get<JwtService>(JwtService);
+		signedToken = jwtService.sign(token, {
+			secret: tokenKey
+		});
+
 		await app.init();
 	});
 
 	it('getUserDetails', () => {
 		return request(app.getHttpServer())
 			.get('/auth/details')
+			.
 			.expect(200, 'TODO');
 	});
 });
