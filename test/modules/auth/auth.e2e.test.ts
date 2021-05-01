f744da9d18c76d9bec38c3dedd9ede30bf0e09b1
Craig <craigmiller160@gmail.com>
2021-05-01 14:52:18 -0400

    Fully refactored test setup logic to be re-usable

--- test/modules/auth/auth.e2e.test.ts
+++ test/modules/auth/auth.e2e.test.ts
@@ -1,47 +1,13 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppModule } from '../../../src/app.module';
 import { INestApplication } from '@nestjs/common';
 import request from 'supertest';
-import { JwkService } from '../../../src/modules/auth/jwk.service';
-import { JwtService } from '@nestjs/jwt';
-import { ConfigService } from '@nestjs/config';
-import {
-	MOCK_CLIENT_KEY,
-	MOCK_CLIENT_NAME,
-	MockConfigService
-} from '../../testutils/mocks/MockConfigService';
-import { MockJwkService } from '../../testutils/mocks/MockJwkService';
-
-const tokenKey = 'TokenKey';
-const token = {
-	clientKey: MOCK_CLIENT_KEY,
-	clientName: MOCK_CLIENT_NAME
-};
+import { createTestingApp } from '../../testutils/e2e/createTestingApp';
 
 describe('AuthController (e2e)', () => {
 	let app: INestApplication;
 	let signedToken: string;
 
 	beforeEach(async () => {
-		const moduleFixture: TestingModule = await Test.createTestingModule({
-			imports: [AppModule]
-		})
-			.overrideProvider(JwkService)
-			.useClass(MockJwkService)
-			.overrideProvider(ConfigService)
-			.useClass(MockConfigService)
-			.compile();
-
-		app = moduleFixture.createNestApplication();
-
-		const jwkService = app.get<JwkService>(JwkService);
-		jwkService.key.next(tokenKey);
-
-		const jwtService = app.get<JwtService>(JwtService);
-		signedToken = jwtService.sign(token, {
-			secret: tokenKey
-		});
-
+		({ app, signedToken } = await createTestingApp());
 		await app.init();
 	});
 
