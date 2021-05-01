9b66220415c9724772a6c9f96acc0614521af18f
Craig <craigmiller160@gmail.com>
2021-05-01 12:15:02 -0400

    Fixed remaining lint errors

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,8 +2,6 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller('/auth')
 export class AuthController {
-	constructor() {}
-
 	@Get('/details')
 	getUserDetails(): string {
 		return 'TODO';
