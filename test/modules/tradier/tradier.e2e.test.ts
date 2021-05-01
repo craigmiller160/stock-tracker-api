a990273ba5407200ca9de763f91ab125109ff74a
Craig <craigmiller160@gmail.com>
2021-05-01 17:06:34 -0400

    Began fixing tradier tests

--- test/modules/tradier/tradier.e2e.test.ts
+++ test/modules/tradier/tradier.e2e.test.ts
@@ -11,70 +11,64 @@ import { format } from 'date-fns';
 import { createTestingApp } from '../../testutils/e2e/createTestingApp';
 import { MockHttpService } from '../../testutils/mocks/MockHttpService';
 
-const createResponse = <T>(data: T): AxiosResponse<T> => ({
-	data,
-	headers: {},
-	config: {},
-	status: 200,
-	statusText: 'OK'
-});
-
 describe('TradierController (e2e)', () => {
 	let app: INestApplication;
 	let mockHttpService: MockHttpService;
 	let signedToken: string;
 
 	beforeEach(async () => {
-		({ app, signedToken } = await createTestingApp());
+		({ app, signedToken, mockHttpService } = await createTestingApp());
 		await app.init();
 	});
 
 	it('GET /tradier/quote/:symbol', async () => {
-		const spy = jest.spyOn(httpService, 'get');
-		spy.mockImplementationOnce(() => of(createResponse(quotes)));
+		mockHttpService.mockResponse(quotes);;
 
 		await request(app.getHttpServer())
 			.get('/tradier/quote/AAPL')
-			.set('Authorization', `Bearer ${token}`)
+			.set('Authorization', `Bearer ${signedToken}`)
 			.expect(200, quotes.quotes.quote);
 
-		expect(spy).toHaveBeenCalledWith(
+		mockHttpService.expectToHaveBeenCalledWith(
+			0,
 			'/markets/quotes?symbols=AAPL',
 			expect.any(Object)
 		);
 	});
 
 	it('GET /tradier/quote/history/:symbol/:date', async () => {
-		const spy = jest.spyOn(httpService, 'get');
-		spy.mockImplementationOnce(() => of(createResponse(historyQuotes)));
-
-		await request(app.getHttpServer())
-			.get('/tradier/quote/history/AAPL/2021-04-27')
-			.set('Authorization', `Bearer ${token}`)
-			.expect(200, historyQuotes.history.day);
-
-		expect(spy).toHaveBeenCalledWith(
-			'/markets/history?symbol=AAPL&interval=monthly&start=2021-04-27&end=2021-04-27',
-			expect.any(Object)
-		);
+		// const spy = jest.spyOn(httpService, 'get');
+		// spy.mockImplementationOnce(() => of(createResponse(historyQuotes)));
+		//
+		// await request(app.getHttpServer())
+		// 	.get('/tradier/quote/history/AAPL/2021-04-27')
+		// 	.set('Authorization', `Bearer ${token}`)
+		// 	.expect(200, historyQuotes.history.day);
+		//
+		// expect(spy).toHaveBeenCalledWith(
+		// 	'/markets/history?symbol=AAPL&interval=monthly&start=2021-04-27&end=2021-04-27',
+		// 	expect.any(Object)
+		// );
+		throw new Error();
 	});
 
 	it('GET /tradier/today/:symbol', async () => {
-		const spy = jest.spyOn(httpService, 'get');
-		spy.mockImplementationOnce(() => of(createResponse(today)));
-
-		await request(app.getHttpServer())
-			.get('/tradier/today/AAPL/')
-			.set('Authorization', `Bearer ${token}`)
-			.expect(200, today.series.data);
-
-		const todayString = format(new Date(), 'yyyy-MM-dd');
-		const start = `${todayString}%2009%3A30`;
-		const end = `${todayString}%2016%3A00`;
-
-		expect(spy).toHaveBeenCalledWith(
-			`/markets/timesales?symbol=AAPL&interval=1min&start=${start}&end=${end}`,
-			expect.any(Object)
-		);
+		// const spy = jest.spyOn(httpService, 'get');
+		// spy.mockImplementationOnce(() => of(createResponse(today)));
+		//
+		// await request(app.getHttpServer())
+		// 	.get('/tradier/today/AAPL/')
+		// 	.set('Authorization', `Bearer ${token}`)
+		// 	.expect(200, today.series.data);
+		//
+		// const todayString = format(new Date(), 'yyyy-MM-dd');
+		// const start = `${todayString}%2009%3A30`;
+		// const end = `${todayString}%2016%3A00`;
+		//
+		// expect(spy).toHaveBeenCalledWith(
+		// 	`/markets/timesales?symbol=AAPL&interval=1min&start=${start}&end=${end}`,
+		// 	expect.any(Object)
+		// );
+		throw new Error();
 	});
 });
