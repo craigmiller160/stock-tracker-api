3ef7013fe3aa9e751b700ef7a995938f188c1442
Craig <craigmiller160@gmail.com>
2021-05-02 07:47:29 -0400

    Added remaining config values to MockConfigService

--- test/testutils/mocks/MockConfigService.ts
+++ test/testutils/mocks/MockConfigService.ts
@@ -1,16 +1,19 @@
 import { ConfigService, NoInferType } from '@nestjs/config';
-import { CLIENT_KEY, CLIENT_NAME, TRADIER_BASE_URL } from '../../../src/config/keys';
+import { AUTH_SERVER_HOST, CLIENT_KEY, CLIENT_NAME, TRADIER_API_KEY, TRADIER_BASE_URL } from '../../../src/config/keys';
 
 export const MOCK_CLIENT_KEY = 'clientKey';
 export const MOCK_CLIENT_NAME = 'clientName';
 export const MOCK_TRADIER_BASE_URL = 'https://tradier.com';
-// TODO make sure all config values are mocked here
+export const MOCK_TRADIER_API_KEY = 'tradierApiKey';
+export const MOCK_AUTH_SERVER_HOST = 'https://authServerHost';
 
 export class MockConfigService extends ConfigService {
 	private config = {
 		[CLIENT_KEY]: MOCK_CLIENT_KEY,
 		[CLIENT_NAME]: MOCK_CLIENT_NAME,
-		[TRADIER_BASE_URL]: MOCK_TRADIER_BASE_URL
+		[TRADIER_BASE_URL]: MOCK_TRADIER_BASE_URL,
+		[TRADIER_API_KEY]: MOCK_TRADIER_API_KEY,
+		[AUTH_SERVER_HOST]: MOCK_AUTH_SERVER_HOST
 	};
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
