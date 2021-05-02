3f9e27c97bad71d808d21e6362124cc6fbfc9259
Craig <craigmiller160@gmail.com>
2021-05-01 22:20:54 -0400

    Added another mock config setting

--- test/testutils/mocks/MockConfigService.ts
+++ test/testutils/mocks/MockConfigService.ts
@@ -1,13 +1,15 @@
 import { ConfigService, NoInferType } from '@nestjs/config';
-import { CLIENT_KEY, CLIENT_NAME } from '../../../src/config/keys';
+import { CLIENT_KEY, CLIENT_NAME, TRADIER_BASE_URL } from '../../../src/config/keys';
 
 export const MOCK_CLIENT_KEY = 'clientKey';
 export const MOCK_CLIENT_NAME = 'clientName';
+export const MOCK_TRADIER_BASE_URL = 'https://tradier.com';
 
 export class MockConfigService extends ConfigService {
 	private config = {
 		[CLIENT_KEY]: MOCK_CLIENT_KEY,
-		[CLIENT_NAME]: MOCK_CLIENT_NAME
+		[CLIENT_NAME]: MOCK_CLIENT_NAME,
+		[TRADIER_BASE_URL]: MOCK_TRADIER_BASE_URL
 	};
 
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
