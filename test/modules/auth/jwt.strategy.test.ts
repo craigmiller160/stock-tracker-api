06091427237095142f17f8cf89c4d07ad785df68
Craig <craigmiller160@gmail.com>
2021-05-02 08:44:14 -0400

    Stubbed out more tests

---
+++ test/modules/auth/jwt.strategy.test.ts
@@ -0,0 +1,15 @@
+describe('jwt.strategy', () => {
+	describe('validate', () => {
+		it('is valid', () => {
+			throw new Error();
+		});
+
+		it('has invalid client key', () => {
+			throw new Error();
+		});
+
+		it('has invalid client name', () => {
+			throw new Error();
+		});
+	});
+});
