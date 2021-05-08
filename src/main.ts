1debba72f6de612c324dcbebb543a973f84212b2
Craig <craigmiller160@gmail.com>
2021-05-08 17:47:22 -0400

    Disabled caching responses

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { httpsOptions } from './tls';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import nocache from 'nocache';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
 		httpsOptions
 	});
+	app.use(nocache());
 	app.disable('x-powered-by');
 	await app.listen(3000);
 }
