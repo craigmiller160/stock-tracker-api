c884fe8620ecb1822be8f8a2c362777f4ff22888
Craig <craigmiller160@gmail.com>
2021-05-08 11:47:21 -0400

    Working to restrict TLS

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import fs from 'fs';
-import path from 'path';
-import { HttpsOptions } from '@nestjs/common/interfaces/external/https-options.interface';
-
-const keyPath = path.resolve(__dirname, 'cert', 'stock-tracker.key.pem');
-const certPath = path.resolve(__dirname, 'cert', 'stock-tracker.cert.pem');
-
-const httpsOptions: HttpsOptions = {
-	key: fs.readFileSync(keyPath),
-	cert: fs.readFileSync(certPath)
-};
+import { httpsOptions } from './tls';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule, {
