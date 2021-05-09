7d007a76f1fe562ea3311bcdadcf2bb25e1a9506
Craig Miller <craigmiller160@gmail.com>
2021-05-09 09:38:48 -0400

    Feature/lint (#7)
    * Removed eslint
    
    * Integrated new linting script
    
    * Fixed linting errors
    
    * Removed yalc dependency

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,13 +1,2 @@
-module.exports = {
-  semi: true,
-  trailingComma: 'none',
-  singleQuote: true,
-  tabWidth: 4,
-  useTabs: true,
-  quoteProps: 'as-needed',
-  jsxSingleQuote: false,
-  bracketSpacing: true,
-  jsxBracketSameLine: false,
-  arrowParens: 'always',
-  endOfLine: 'lf'
-};
+
+module.exports = require('@craigmiller160/es-lint-config/lint/.prettierrc.js');
