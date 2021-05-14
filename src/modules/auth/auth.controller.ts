664b6dff9fe861b98c9924af05fead0b64cd110c
Craig Miller <craigmiller160@gmail.com>
2021-05-14 15:34:26 -0400

    Renamed user details path to match spring boot

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { TokenDetails } from './model/jwt';
 import { TokenDetailsService } from './token-details.service';
 
-@Controller('/auth')
+@Controller('/oauth')
 export class AuthController {
 	constructor(private readonly tokenDetailsService: TokenDetailsService) {}
 
-	@Get('/details')
+	@Get('/user')
 	getUserDetails(): TokenDetails {
 		return this.tokenDetailsService.getTokenDetails();
 	}
