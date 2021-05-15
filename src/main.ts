cd035f645aba2ffb5c837c0b13cb9cab2069e8b4
Craig Miller <craigmiller160@gmail.com>
2021-05-15 16:59:34 -0400

    Integrated session

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { httpsOptions } from './tls';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import nocache from 'nocache';
+import session from './session';
 
 async function bootstrap() {
 	const app = await NestFactory.create<NestExpressApplication>(AppModule, {
 		httpsOptions
 	});
 	app.use(nocache());
+	app.use(session);
 	app.disable('x-powered-by');
 	await app.listen(8080);
 }
